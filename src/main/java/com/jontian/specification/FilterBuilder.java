a5cf8cd1a1b03fbfe80376cf96257406694ba4c8
zhongjun <zhongjun.tian@utdallas.edu>
2017-02-25 15:46:28 -0600

    refactor code

---
+++ src/main/java/com/jontian/specification/FilterBuilder.java
@@ -0,0 +1,16 @@
+package com.jontian.specification;
+
+/**
+ * Created by Jon Tian on 2/25/17.
+ */
+public class GenericSpecificationBuilder {
+//TODO
+    Filter filter;
+    public GenericSpecificationBuilder(){
+        filter = new Filter();
+    };
+    public Filter and(){
+
+    }
+
+}
