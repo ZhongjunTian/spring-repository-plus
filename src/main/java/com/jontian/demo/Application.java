38ec86c9f46f59d97e33b60c3c9c05fec7ad8bb1
zhongjun <zhongjun.tian@utdallas.edu>
2017-02-25 14:46:30 -0600

    first demo version, with all example code.

--- src/main/java/com/jontian/demo/Application.java
+++ src/main/java/com/jontian/demo/Application.java
@@ -1,12 +1,12 @@
-package com.jontian.demo;
+package com.jontian.demo.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
