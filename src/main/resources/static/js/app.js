cc245263b6462f1cd358c18a98eb4bf001421b4b
Luke Li <leezhc@gmail.com>
2017-03-08 16:19:03 -0800

    UI/3.8-1

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.controller('springCtrl', function($scope, $http) {
+    $scope.getMain = function() {
+        $http.get('/').success(function(res) {
+            $scope.mainRes = res;
+            console.log($scope.mainRes);
+        })
+    }
+    $scope.getMain();
+});
+app.controller('joinCtrl', function($scope, $http) {
+    $scope.getJoin = function() {
+        $http.get('/join').success(function(res) {
+            $scope.joinRes = res;
+            console.log($scope.joinRes);
+        })
+    };
+    $scope.getJoin();
+});
+app.controller('filtCtrl', function($scope, $http) {
+    $scope.fieldF = '';
+    $scope.operatorF = '';
+    $scope.valueF = '';
+    $scope.dataFilt = {
+
+    };
+
+    $scope.doFilt = function() {
+        $scope.dataF = JSON.stringify($scope.dataFilt);
+        console.log($scope.dataF);
+
+        $http.post('/filter', $scope.dataF).success(function(res) {
+            $scope.filtRes = res;
+            console.log($scope.filtRes);
+        });
+    };
+
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "./page/main.html",
+            controller: "springCtrl"
+        })
+        .when("/join", {
+            templateUrl: "./page/join.html",
+            controller: "joinCtrl"
+        }).
+    when("/filt", {
+        templateUrl: "./page/filt.html",
+        controller: "filtCtrl"
+    })
+});
