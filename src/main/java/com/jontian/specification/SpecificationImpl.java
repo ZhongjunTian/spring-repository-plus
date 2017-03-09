0f7bc6e2e1d16fecece04f8b79d92ce3a38aa0cf
zhongjun <zhongjun.tian@utdallas.edu>
2017-03-08 19:16:29 -0600

    add apache 2.0

--- src/main/java/com/jontian/specification/SpecificationImpl.java
+++ src/main/java/com/jontian/specification/SpecificationImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jontian.specification;
 
 import com.jontian.specification.exception.SpecificationException;
@@ -12,6 +27,9 @@ import java.util.List;
 
 import static com.jontian.specification.Filter.*;
 
+/**
+ *@author Jon (Zhongjun Tian)
+ */
 public class SpecificationImpl implements Specification<Object> {
     private static final Logger logger = LoggerFactory.getLogger(SpecificationImpl.class);
     private Filter filter;
